a69f645eb2ad10c1d528768308663f17c96f1334
Boeasy <63324374+Boeasy@users.noreply.github.com>
2024-06-01 15:11:09 -0600

    starting hw3 and project 1...

--- Homeworks/HW3/HW3Arrays/Program.cs
+++ Homeworks/HW3/HW3Arrays/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace HW3Arrays
+{
+    public class HW3Arrays
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Create 1D array of 10 random numbers between 0-100 using random class, create an instance of the class.
+             * use a for loop to populate array with instance.Next(100).
+             * Print out the array, sort it, print it out again
+             * reverse the array, print out again
+             *
+             * create a 3x3 rectangular array with some values.
+             * create a jagged array with at least 3 rows.
+             */
+        }
+    }
+}
---
+++ Projects/Project1/BirthdayCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BirthdayCalculator
+{
+    /*
+     * Ask User for their birthday
+     * calculate their age
+     * make sure age is 'possible'
+     * output age to console
+     * if birthday, output a nice message
+     * compute astrological sign according to western and chinese astrology systems
+     * output computed signs to console, optionally output additional information based on sign.
+     */
+    public class BirthdayCalculator
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
